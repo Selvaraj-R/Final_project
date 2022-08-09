bd4edd882bed09d52baa45f6e00694d87877ae26
Selvaraj-R <selvakaran9176@gmail.com>
2022-08-09 16:06:21 +0530

    worked on static page to list course

--- KODO2.0/config/routes.rb
+++ KODO2.0/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'courses/index'
   get 'home/index'
-
+  get 'courses/content'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
